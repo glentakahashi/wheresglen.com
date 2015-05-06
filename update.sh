d9baf1174768441b79b1af8971ce3dca29c9e835
Glen Takahashi <me@glentaka.com>
2015-05-06 16:44:07 +0000

    Updates to update script, interface, google analytics

--- update.sh
+++ update.sh
@@ -9,6 +9,9 @@ OUTFILE=data.json
 if wget -O $INFILE -x --load-cookies cookie.txt "https://maps.google.com/locationhistory/b/0/kml?startTime=$START&endTime=$NOW"
 then
   node parse.js $INFILE $OUTFILE
+  cp index.html /var/www/wheresglen.com/
+  cp style.css /var/www/wheresglen.com/
+  cp data.json /var/www/wheresglen.com/
 else
   echo "Couldn't get location history"
   return 1
