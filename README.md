acf38589ccd931214eaffb699d8ee067c2466254
Glen Takahashi <me@glentaka.com>
2015-05-06 20:05:57 +0400

    Really rough v0.0.1

--- README.md
+++ README.md
@@ -8,3 +8,17 @@ points:
   use the location, country, etc to name the point
   use the start and end dates of the first and last point
   another table with keeps track of the first date downloaded
+
+three parts
+1. Establish the major "trip" which just details overnight stays and identifies using a red line
+  do this by seeing where I was on average between the hours of 10pm and 8am (OF THE TIME ZONE OF THE LOCATION) or something
+
+2. Establish the activites/attractions visited from those places
+  make these like "pins" on the map
+3. Show the last 3 hours (or maybe 100 points) of detailed locations that I am in
+4. Show images/facebook statuses/etc from the thing
+5. Only do this if you are a facebook friend.
+  make them "Log in with facebook" and use a backend api to determine if they are friends with my id
+  only show the details/photos/statuses/last if they are friends with me
+
+add controls to browse through my history
--- parse.js
+++ parse.js
@@ -24,9 +24,9 @@ var tj = require('togeojson'),
     var currloc = { "locs": [[0,0,0,-1]] };
 var d = 0;
 //to unbundle distance must be more than 5 miles different
-var MAX_DIST = 25;
+var MAX_DIST = config.maxDistance;
 // have to be at a spot a minimum of 3 hours
-var MIN_TIME = 1000 * 60 * 60 * 3;
+var MIN_TIME = config.minTime;
 for (i = 0;i<ts.length;i++) {
   ts[i] = Date.parse(ts[i]);
 }
@@ -71,22 +71,9 @@ for (i = 0; i<ps.length;i++) {
       "lon": 0,
       "start": date,
       "end": 0,
-      "loc": ""
+      "loc": null,
+      "daytrip": false
     };
-    //TODO: rate limit
-    //var options = {
-    //host: 'maps.googleapis.com',
-    //path: '/maps/api/geocode/json?latlng='+ps[i][1]+','+ps[i][0]+'&key='+config.API_KEY
-    //};
-
-    //https.get(options, function(res) {
-    //console.log("Got response: " + res.statusCode);
-    //res.on('data', function(d) {
-    //process.stdout.write(d);
-    //});
-    //}).on('error', function(e) {
-    //console.log("Got error: " + e.message);
-    //});
   } else {
     q = ps[i];
     q.push(i);
@@ -114,12 +101,63 @@ if (currloc['start'] !== undefined) {
   date = currloc['start'];
 }
 
-//TODO: merge points via cities now
-
 //delete the random JFK point that appears
 locs.splice(1,1)
 //fix the times now
 locs[0].end = locs[1].start
+
+var finish = function() {
+  for(i = 0;i<locs.length;i++) {
+    for(j = i+1;j<locs.length;j++) {
+      if(locs[i].loc == locs[j].loc) {
+        for(k = i+1; k < j;k++) {
+          locs[k].daytrip = true;
+        }
+        locs[i].end = locs[j].end;
+        locs.splice(j,1);
+        j--;
+      }
+    }
+  }
+  fs.writeFileSync(process.argv[3], JSON.stringify(locs,null,2));
+}
+
+var count = locs.length;
+for(i = 0;i<locs.length;i++) {
+  //TODO: rate limit
+  var getInfo = function(z) {
+    var options = {
+      host: 'maps.googleapis.com',
+      path: '/maps/api/geocode/json?latlng='+locs[z].lat+','+locs[z].lon+'&result_type=locality&key='+config.API_KEY
+    };
+
+    https.get(options, function(res) {
+      //console.log("Got response: " + res.statusCode);
+      var data = "";
+      res.on('data', function(d) {
+        data += d;
+      });
+      res.on('end', function() {
+        d = JSON.parse(data);
+        if(d['status'] !== undefined && d['status'] == 'OVER_QUERY_LIMIT') {
+          setTimeout(function() {getInfo(z)}, 2000);
+        } else {
+          if(d.results.length > 0) {
+            locs[z].loc = d.results[d.results.length-1].formatted_address;
+          }
+          count--;
+          if(count == 0) {
+            finish();
+          }
+        }
+      });
+      }).on('error', function(e) {
+        console.log("Got error: " + e.message);
+    });
+  }
+  getInfo(i);
+}
+
 
 //http://stackoverflow.com/questions/27928/how-do-i-calculate-distance-between-two-latitude-longitude-points
 function distance(lat1, lon1, lat2, lon2) {
@@ -133,4 +171,3 @@ function distance(lat1, lon1, lat2, lon2) {
   return R * 2 * Math.asin(Math.sqrt(a));
 }
 
-fs.writeFileSync(process.argv[3], JSON.stringify(locs,null,2));
